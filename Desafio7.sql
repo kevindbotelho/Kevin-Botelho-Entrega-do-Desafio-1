22449dba67d35e3cac9bdfccd3604712e42373cc
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:38 -0300

    Update Desafio7.sql

--- Desafio7.sql
+++ Desafio7.sql
@@ -1,4 +1,7 @@
+-- Desafio 7: Média de produtos vendidos;
 SELECT
     AVG(Quantidade) AS MediaProdutosVendidos
+-- Calcula a média da quantidade de produtos vendidos
 FROM
     dbo.FatoDetalhes;
+-- Utiliza a tabela FatoDetalhes que contém informações detalhadas sobre as vendas
