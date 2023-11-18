27874170d3d3c9ef9f432f962c09ff3747b68494
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:31 -0300

    Update Desafio4.sql

--- Desafio4.sql
+++ Desafio4.sql
@@ -1,9 +1,14 @@
+-- Desafio 4: Lucro dos meses;
 SELECT
     FORMAT(Cabecalho.Data, 'yyyy-MM') AS Mes,
     SUM(Detalhes.ValorLiquido) AS LucroTotal
+-- Formata a data para o formato 'yyyy-MM'
+-- Soma dos valores líquidos para cada mês
 FROM
     dbo.FatoDetalhes AS Detalhes
 JOIN
     dbo.FatoCabecalho AS Cabecalho ON Detalhes.CupomID = Cabecalho.CupomID
+-- Junção com a tabela FatoCabecalho para obter informações sobre a data
 GROUP BY
     FORMAT(Cabecalho.Data, 'yyyy-MM');
+-- Agrupamento pelo mês formatado para agregar os resultados
