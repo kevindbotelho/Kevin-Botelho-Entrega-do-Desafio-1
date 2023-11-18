b55a8848e865f71aab5c3a3f590494741e3d993d
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:27 -0300

    Update Desafio2.sql

--- Desafio2.sql
+++ Desafio2.sql
@@ -1,9 +1,13 @@
+-- Desafio 2: Valor de venda por tipo de produto;
 SELECT
     Produtos.Produto,
     SUM(Detalhes.ValorLiquido) AS ValorTotalVendas
+-- Soma dos valores líquidos para cada tipo de produto
 FROM
     dbo.FatoDetalhes AS Detalhes
 JOIN
     dbo.Dimensoes_Dados_Produtos AS Produtos ON Detalhes.ProdutoID = Produtos.ProdutoID
+-- Junção com a tabela Dimensoes_Dados_Produtos para obter informações sobre o produto
 GROUP BY
     Produtos.Produto;
+-- Agrupamento pelo campo Produto para agregar os resultados
