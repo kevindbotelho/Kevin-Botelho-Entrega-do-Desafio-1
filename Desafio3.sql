d41d3a4dbd37015e0095c7313b020bf99bee03b3
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:29 -0300

    Update Desafio3.sql

--- Desafio3.sql
+++ Desafio3.sql
@@ -1,10 +1,16 @@
+-- Desafio 3: Quantidade e valor das vendas por dia, mês, ano;
 SELECT
     FORMAT(Cabecalho.Data, 'yyyy-MM-dd') AS Data,
     SUM(Detalhes.Quantidade) AS QuantidadeTotal,
     SUM(Detalhes.ValorLiquido) AS ValorTotalVendas
+-- Formata a data para o formato 'yyyy-MM-dd'
+-- Soma das quantidades vendidas para cada data
+-- Soma dos valores líquidos para cada data
 FROM
     dbo.FatoDetalhes AS Detalhes
 JOIN
     dbo.FatoCabecalho AS Cabecalho ON Detalhes.CupomID = Cabecalho.CupomID
+-- Junção com a tabela FatoCabecalho para obter informações sobre a data
 GROUP BY
     FORMAT(Cabecalho.Data, 'yyyy-MM-dd');
+-- Agrupamento pela data formatada para agregar os resultados
