ee72b95f2e31c33e9d0443c7dd455ff78a2c72cd
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:41 -0300

    Update Desafio8.sql

--- Desafio8.sql
+++ Desafio8.sql
@@ -1,12 +1,16 @@
+-- Desafio 8: Média de compras que um cliente faz;
 SELECT
     AVG(NumCompras) AS MediaComprasPorCliente
+-- Calcula a média do número de compras distintas por cliente
 FROM
     (
         SELECT
             ClienteID,
             COUNT(DISTINCT CupomID) AS NumCompras
+        -- Conta o número de compras distintas por cliente
         FROM
             dbo.FatoCabecalho
         GROUP BY
             ClienteID
     ) AS ComprasPorCliente;
+-- Utiliza uma subconsulta para contar o número de compras distintas por cliente
