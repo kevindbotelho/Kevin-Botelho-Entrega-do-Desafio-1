7a0ab8bafded883b8846566e54025331e583cd11
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:36 -0300

    Update Desafio6.sql

--- Desafio6.sql
+++ Desafio6.sql
@@ -1,11 +1,15 @@
+-- Desafio 6: Venda por cliente, cidade do cliente e estado;
 SELECT
     Clientes.Cliente,
     Clientes.Cidade,
     Clientes.PaisCodigo AS Estado,
     SUM(Cabecalho.ValorFrete) AS ValorTotalVendas
+-- Soma dos valores de frete para cada cliente, cidade e estado
 FROM
     dbo.FatoCabecalho AS Cabecalho
 JOIN
     dbo.Dimensoes_Dados_Clientes AS Clientes ON Cabecalho.ClienteID = Clientes.ClienteID
+-- Junção com a tabela Dimensoes_Dados_Clientes para obter informações sobre o cliente
 GROUP BY
     Clientes.Cliente, Clientes.Cidade, Clientes.PaisCodigo;
+-- Agrupamento pelo cliente, cidade e estado para agregar os resultados
