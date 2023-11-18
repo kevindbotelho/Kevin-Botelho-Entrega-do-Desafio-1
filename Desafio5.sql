59ef27133329f48a60640e1a8328677a74caac3d
kevindbotelho <kevindinizbotelho@gmail.com>
2023-11-18 13:00:34 -0300

    Update Desafio5.sql

--- Desafio5.sql
+++ Desafio5.sql
@@ -1,9 +1,13 @@
+-- Desafio 5: Venda por produto;
 SELECT
     Produtos.Produto,
     SUM(Detalhes.Quantidade) AS QuantidadeTotalVendida
+-- Soma das quantidades vendidas para cada produto
 FROM
     dbo.FatoDetalhes AS Detalhes
 JOIN
     dbo.Dimensoes_Dados_Produtos AS Produtos ON Detalhes.ProdutoID = Produtos.ProdutoID
+-- Junção com a tabela Dimensoes_Dados_Produtos para obter informações sobre o produto
 GROUP BY
     Produtos.Produto;
+-- Agrupamento pelo produto para agregar os resultados
